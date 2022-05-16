4b8fbab7f619708fc5ce436b60193454c76d9cb1
Chacal <sylvainlorent@gmail.com>
2022-05-16 15:17:58 +0200

    [AddSeparator] First Step
    • If there is no layer selected: the script creates a shape layer on top of all layers called "----".
    • If one of several layers are selected: the script creates a shape layer on top of the highest layer in pile called "----".

--- CTboxElements/Misc/addSeparator.jsx
+++ CTboxElements/Misc/addSeparator.jsx
@@ -8,5 +8,58 @@
  */
 function addSeparator(){
     var modifiers = modifiersStatuses() ;
-    alert( modifiers.ctrlState + " / " + modifiers.majState + " / " + modifiers.altState );
+    var layerSelection = CTcheckSelectedLayers();
+    var separator = null ;
+    app.beginUndoGroup("Add Separator.")
+    if( layerSelection.length > 0 ){
+        if( !modifiers.ctrlState && !modifiers.majState && !modifiers.altState ){
+            var highestLayer = layerSelection[0];
+            if( layerSelection.length > 1 ){
+                highestLayer = sortLayersByIndex( layerSelection )[0];
+            }
+            separator = createShape();
+            separator.name = "----------------------------------------";
+            separator.moveBefore( highestLayer );
+        }else if( modifiers.ctrlState && !modifiers.majState && !modifiers.altState ){
+            alert("ctrl");
+        }else if( !modifiers.ctrlState && modifiers.majState && !modifiers.altState ){
+            alert("maj");
+        }else if( !modifiers.ctrlState && !modifiers.majState && modifiers.altState ){
+            alert("alt");
+        }else if( modifiers.ctrlState && modifiers.majState && !modifiers.altState ){
+            alert("ctrl + maj");
+        }else if( !modifiers.ctrlState && modifiers.majState && modifiers.altState ){
+            alert("maj+alt");
+        }else if( modifiers.ctrlState && !modifiers.majState && modifiers.altState ){
+            alert("ctrl+alt");
+        }else if( modifiers.ctrlState && modifiers.majState && modifiers.altState ){
+            alert( "ctrl+maj+alt");
+        }
+    } else {
+        separator = createShape()
+        separator.name = "----------------------------------------";
+    }
+    app.endUndoGroup();
+}
+/**
+ * Create the Shape layer for Separator
+ */
+function createShape(){
+
+    var shapeLayer = app.project.activeItem.layers.addShape();
+    shapeLayer.property( "ADBE Transform Group" ).property( "ADBE Position" ).setValue( [ -50 , -50 ] );
+    shapeLayer.guideLayer = true ;
+    shapeLayer.label = 0 ;
+    return shapeLayer ;
+
+}
+/**
+ * Sort an array of layers according to their index
+ * @param { Array } Array containing the layers to sort.
+ */
+function sortLayersByIndex( layers ){
+
+    var sortedLayers = layers.sort( function( a , b ){ return parseFloat( a.index ) - parseFloat( b.index ) } );
+    return sortedLayers ;
+
 }
