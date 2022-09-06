99d2953c2bf390b53079683ea0d51f014865ba88
Chacal <sylvainlorent@gmail.com>
2022-09-06 09:46:08 +0200

    [UpdateExp]French Removal

--- CTboxElements/Expressions/UpdateExp.jsx
+++ CTboxElements/Expressions/UpdateExp.jsx
@@ -8,43 +8,43 @@
  */
 function updateExpChoice(){
     
-    var MAJexpDlg = new Window( "palette" , { en: "Expression Modifier" ,  fr: "Modificateur d'Expressions." } );
-    MAJexpDlg.global = MAJexpDlg.add( "Panel" , undefined , { en: "Upd. Expression : " , fr: "MAJ Expression : " } );
+    var MAJexpDlg = new Window( "palette" , "Expression Modifier" );
+    MAJexpDlg.global = MAJexpDlg.add( "Panel" , undefined , "Upd. Expression : " );
     MAJexpDlg.global.alignChildren = "Fill" ;
         //Creating a first Row to define the text to replace.
         MAJexpDlg.row1 = MAJexpDlg.global.add( "Group" );
         MAJexpDlg.row1.orientation = "row" ;
         MAJexpDlg.row1.alignchildren = "Left" ;
-            var row1Text = MAJexpDlg.row1.add( "StaticText" , undefined , { en: 'Text to Replace : ' , fr: 'Texte à Remplacer : ' } );
+            var row1Text = MAJexpDlg.row1.add( "StaticText" , undefined , "Text to Replace : " );
             row1Text.characters = 11 ;
             var textToReplace = MAJexpDlg.row1.add( "EditText{ justify : 'right' , characters : 15 , properties : { enabled : true } }" );
-            textToReplace.text = { en: "Text to Replace" , fr: "Texte à Remplacer" };
+            textToReplace.text = "Text to Replace" ;
         //Creating a second Row to define the text to replace with.
         MAJexpDlg.row2 = MAJexpDlg.global.add( "Group" );
         MAJexpDlg.row2.orientation = "row" ;
         MAJexpDlg.row2.alignchildren = "Left" ;
-            var row2Text = MAJexpDlg.row2.add( "StaticText" , undefined , { en: 'Replace by : ' , fr: 'Remplacer par : ' } );
+            var row2Text = MAJexpDlg.row2.add( "StaticText" , undefined , "Replace by : " );
             row2Text.characters = 11 ;
             var replacementText = MAJexpDlg.row2.add( "EditText{ justify : 'right' , characters : 15 , properties : { enabled : true } }" );
-            replacementText.text = { en: 'Text to Use' , fr: 'Texte à Utiliser' };
+            replacementText.text = 'Text to Use' ;
         //Creating a third Row to choose if the script will work trough layers or properties.
         MAJexpDlg.row3 = MAJexpDlg.global.add( "Group" );
         MAJexpDlg.row3.orientation = "row" ;
         MAJexpDlg.row3.alignment = "center" ;
-            var onLayers = MAJexpDlg.row3.add( "radiobutton" , undefined , { en : " - by Layer." , fr: " - par Calque." } );
+            var onLayers = MAJexpDlg.row3.add( "radiobutton" , undefined , " - by Layer." );
             onLayers.characters = 10 ;
             onLayers.value = true ;
-            var onProps = MAJexpDlg.row3.add( "radiobutton" , undefined , { en: " - by Properties." , fr: " - par Propriétés." } );
+            var onProps = MAJexpDlg.row3.add( "radiobutton" , undefined , " - by Properties." );
             onProps.characters = 10 ;
         //Creating a fourth Row for the Buttons.
         MAJexpDlg.Btns = MAJexpDlg.global.add( "Group" )
         MAJexpDlg.Btns.orientation = "row" ;
         MAJexpDlg.Btns.alignment = "Center" ;
-            var replace = MAJexpDlg.Btns.add( "Button" , undefined , { en: "Replace" , fr: "Remplacer" } );
-            var cancel = MAJexpDlg.Btns.add( "Button" , undefined , { en: "Cancel" , fr: "Annuler"} );
+            var replace = MAJexpDlg.Btns.add( "Button" , undefined , "Replace" );
+            var cancel = MAJexpDlg.Btns.add( "Button" , undefined , "Cancel" );
     //UI Events.
-    textToReplace.onActivate = function(){ if( textToReplace.text == { en: "Text to Replace" , fr: "Texte à Remplacer" } ){ textToReplace.text = "" ; } } ;
-    replacementText.onActivate = function(){ if( replacementText.text == { en: "Text to Use" , fr: "Texte à Utiliser" } ){ replacementText.text = "" ; } } ;
+    textToReplace.onActivate = function(){ if( textToReplace.text == "Text to Replace" ){ textToReplace.text = "" ; } } ;
+    replacementText.onActivate = function(){ if( replacementText.text == "Text to Use" ){ replacementText.text = "" ; } } ;
     replace.onClick = function(){ updateExp( textToReplace.text , replacementText.text , onLayers.value ); } ;
     cancel.onClick = function(){ MAJexpDlg.close() } ;
     //Showing UI
@@ -64,11 +64,11 @@ function updateExp( textA , textB , worksOnLayers , showEndAlert , createUndoGro
     if( typeof createUndoGroup === "undefined" ){ createUndoGroup = true ; }
     
     //Checking if the inputs are usable.
-    if( textA == { en: "Text to Replace" , fr: "Texte à Remplacer" } || textA == "" ){
-        CTalertDlg( { en: "Error : " , fr: "Erreur : " } , { en: "   You did not tell me what to replace" , fr: "   Tu ne m'a pas dis quoi remplacer." } );
+    if( textA == "Text to Replace" || textA == "" ){
+        CTalertDlg( "Error : " , "   You did not tell me what to replace" );
         return ;
-    } else if( textB ==  { en: "Text to Use" , fr: "Texte à Utiliser" } ){
-        CTalertDlg( { en: "Error : " , fr: "Erreur : " } , { en: "   You did not tell me what to replace with." , fr: "   Tu ne m'a pas dis par quoi remplacer." } );
+    } else if( textB ==  "Text to Use" ){
+        CTalertDlg( "Error : " , "   You did not tell me what to replace with." );
         return ;
     }
     //Starting the actual work.
@@ -77,14 +77,14 @@ function updateExp( textA , textB , worksOnLayers , showEndAlert , createUndoGro
         if( layersToUpdate.length > 0 ){
             for( var i = 0 ; i < layersToUpdate.length ; i++ ){
                 //Opening the UndoGroup.
-                if( createUndoGroup ){ app.beginUndoGroup( { en: "Expression Modification" , fr: "Modification de l'Expression" } ); }
+                if( createUndoGroup ){ app.beginUndoGroup( "Expression Modification" , "Modification de l'Expression" ); }
                 //Correcting the Expressions.
                 applyExpUpdate( layersToUpdate[i] , textA , textB ) ;
                 //Closing the UndoGroup.
                 if( createUndoGroup ){ app.endUndoGroup(); }
             }
             if( showEndAlert ){
-                CTalertDlg( { en: "I'm Done" , fr: "J'ai Fini" } , { en: "I've finished updating your expressions" , fr: "   J'ai fini de mettre à jour tes expressions." } );
+                CTalertDlg( "I'm Done" , "I've finished updating your expressions" );
             }
         }
     } else {
@@ -94,7 +94,7 @@ function updateExp( textA , textB , worksOnLayers , showEndAlert , createUndoGro
                 var currentProperty = CTgetProperty( propertiesToTreat[i] );
                 if( currentProperty.canSetExpression ){
                     //Opening the UndoGroup.
-                    if( createUndoGroup ){ app.beginUndoGroup( {en: "Expression Modification" , fr: "Modification de l'Expression" } ); }
+                    if( createUndoGroup ){ app.beginUndoGroup( "Expression Modification" ); }
                     //Correcting the Expressions.
                     var reg = new RegExp( textA.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&'), "g");//"$&", in the regExp, means "the String found". So, in our case, it means one of the special characters in the list will be replaced by himself preceeded by a "\".
                     currentProperty.expression = currentProperty.expression.replace( reg , textB );
@@ -103,7 +103,7 @@ function updateExp( textA , textB , worksOnLayers , showEndAlert , createUndoGro
                 }
             }
             if( showEndAlert ){
-                CTalertDlg( { en: "I'm Done" , fr: "J'ai Fini" } , { en: "I've finished updating your expressions" , fr: "   J'ai fini de mettre à jour tes expressions." } );
+                CTalertDlg( "I'm Done" , "I've finished updating your expressions" );
             }
         }
     }
